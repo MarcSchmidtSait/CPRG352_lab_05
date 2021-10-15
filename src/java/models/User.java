47fba749ec793556894e3ea3529f83b8efad2f6e
854638 <854638@SAIT227569IT.ACDM.DS.SAIT.CA>
2021-10-15 15:34:03 -0600

    Finished Lab

---
+++ src/java/models/User.java
@@ -0,0 +1,36 @@
+
+package models;
+
+import java.io.Serializable;
+
+
+public class User implements Serializable{
+    private String username;
+    private String password;
+    
+    public User(){
+            this.username = "";
+            this.password = "";
+    }
+    
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;       
+    }
+    
+    public void setUserName(String username){
+        this.username = username;
+    }
+    
+    public void setPassword(String password){
+        this.password = password;
+    }
+    
+    public String getUsername(){
+        return username;
+    }
+    
+    public String getPassword(){
+        return password;
+    }
+}
